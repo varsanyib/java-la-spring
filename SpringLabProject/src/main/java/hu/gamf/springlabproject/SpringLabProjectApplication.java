decf4293aefabbe90e76293ed4bd8eba7a9694a7
Varsányi Barnabás <varsanyib0125@gmail.com>
2025-11-25 18:05:36 +0100

    Base project added

---
+++ SpringLabProject/src/main/java/hu/gamf/springlabproject/SpringLabProjectApplication.java
@@ -0,0 +1,13 @@
+package hu.gamf.springlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabProjectApplication.class, args);
+    }
+
+}
---
+++ SpringLabProject/src/test/java/hu/gamf/springlabproject/SpringLabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gamf.springlabproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
